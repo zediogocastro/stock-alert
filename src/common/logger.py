39b09ef9f8240e1b506a8372c424a3994ed9fe07
José Castro <zediogo2@gmail.com>
2025-12-16 00:01:23 +0000

    addds logger

---
+++ src/common/logger.py
@@ -0,0 +1,42 @@
+import logging
+import os
+
+from colorlog import ColoredFormatter
+
+def __get_logger() -> logging.Logger:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # Creates a stream handler
+    handler = logging.StreamHandler()
+    fomatter = ColoredFormatter(
+        fmt=(
+        "%(log_color)s%(asctime)s.%(msecs)03d [%(levelname)-8s]%(reset)s "
+        "%(cyan)s%(name)s%(reset)s.%(funcName)s:%(lineno)d - "
+        "%(message_log_color)s%(message)s"
+        ),
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+        secondary_log_colors={
+                "message": {
+                "DEBUG": "white",
+                "INFO": "white",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red",
+            }
+        },
+        reset=True
+    )
+    handler.setFormatter(fomatter)
+    logger.addHandler(handler)
+
+    return logger
+
+logger = __get_logger()
