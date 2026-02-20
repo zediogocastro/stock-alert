b020e1bc6eb3208a71860aa2670d82b83aa15222
José Castro <zediogo2@gmail.com>
2026-02-20 00:14:14 +0000

    feat(pipeline): creates master table

--- scripts/run_stock_check.py
+++ scripts/run_stock_check.py
@@ -1,5 +1,6 @@
 from stock_alert import YFinanceFetcher, DataPipeline
 from stock_alert.features import FeatureEngine, MovingAverage
+from stock_alert.features.atomic_features import Returns, Volatility, RelativeStrengthIndex
 #from stock_alert.exporter import PlotExporter, CSVExporter, CompositeExporter
 
 from common.logger import logger
@@ -18,7 +19,9 @@ COLUMN = "Close"
 # Exporters
 REPORTS_DIR = "reports"
 
-# Display preference
+# General
+MASTER_TABLE_CACHE = "data/transformed"
+
 
 #---------------------## End ##---------------------#
 
@@ -35,6 +38,9 @@ if __name__ == "__main__":
         MovingAverage(column=COLUMN, window_days=21, sort_by=SORT_BY, group_by=IDENTIFIER),
         MovingAverage(column=COLUMN, window_days=50, sort_by=SORT_BY, group_by=IDENTIFIER),
         MovingAverage(column=COLUMN, window_days=200, sort_by=SORT_BY, group_by=IDENTIFIER),
+        #Returns(column=COLUMN, n_days=1, sort_by=SORT_BY, group_by=IDENTIFIER),
+        #Volatility(column=COLUMN, window_days=21, sort_by=SORT_BY, group_by=IDENTIFIER),
+        #RelativeStrengthIndex(column=COLUMN, window_days=14, sort_by=SORT_BY, group_by=IDENTIFIER)
     ]
     feature_engine = FeatureEngine(features=features_to_copute)
 
@@ -54,6 +60,6 @@ if __name__ == "__main__":
     # ])
 
     # Initialize and run pipeline
-    pipeline = DataPipeline(fetcher, feature_engine)
+    pipeline = DataPipeline(fetcher, feature_engine, master_table_directory=MASTER_TABLE_CACHE)
     pipeline.run()
     logger.info("✅ Pipeline Completed!")
--- src/stock_alert/pipeline.py
+++ src/stock_alert/pipeline.py
@@ -1,4 +1,5 @@
 import polars as pl
+from pathlib import Path
 from common.logger import logger
 from stock_alert.fetcher import BaseFetcher
 #from stock_alert.transformer import Transformer
@@ -10,10 +11,12 @@ class DataPipeline:
     def __init__(self, 
                  fetcher: BaseFetcher, 
                  feature_engine: FeatureEngine, 
-                 exporter: BaseExporter | None = None):
+                 exporter: BaseExporter | None = None,
+                 master_table_directory: str | None = None):
         self.fetcher = fetcher
         self.feature_engine = feature_engine
         self.exporter = exporter
+        self.master_table_directory = master_table_directory
 
     def run(self) -> None:
         try:
@@ -30,6 +33,10 @@ class DataPipeline:
             data = pl.from_pandas(data).lazy()
             transformed = self.feature_engine.transform(data)
 
+            if self.master_table_directory:
+                master_table_path = Path(self.master_table_directory) / "master_table.parquet"
+                self._save_data(transformed, master_table_path)
+
             # Export
             if self.exporter: 
                 logger.info("Exporting data...")
@@ -40,5 +47,16 @@ class DataPipeline:
                 
         except Exception as e:
             raise RuntimeError(f"Pipeline failed: {e}") from e
+        
+    def _save_data(self, data: pl.LazyFrame, path: Path) -> None:
+        """Save data to a path.
+        
+        Args:
+            data:  Lazy Polars Data to be saved.
+            direcotry: The path of the file to be saved into.
+        """
+        path.parent.mkdir(parents=True, exist_ok=True)  
+        data.sink_parquet(path)
+        logger.info(f"Saved raw data to {path}")
         
 
