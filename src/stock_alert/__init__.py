e9e65c55c99d9bd96465f0f38af79c575f6c143a
José Castro <zediogo2@gmail.com>
2026-03-10 23:46:13 +0000

    feat(pipeline): refactor data fetching and feature generation with new services, closes #7

--- src/stock_alert/__init__.py
+++ src/stock_alert/__init__.py
@@ -1,6 +1,6 @@
 from .fetcher import BaseFetcher, YFinanceFetcher, EuriborFetcher
 from .features import Feature, FeatureEngine, MovingAverage
-from .pipeline import DataPipeline
+from .pipeline import FetcherService, FeatureService
 
 __version__ = "0.1.0"
 
@@ -11,5 +11,6 @@ __all__ = [
     "Feature",
     "FeatureEngine",
     "MovingAverage",
-    "DataPipeline",
+    "FetcherService",
+    "FeatureService",
 ]
