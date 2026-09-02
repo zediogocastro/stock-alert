3aeccfdd63598ddd6911d49c51d0e785e4ea5c61
José Castro <zediogo2@gmail.com>
2026-09-03 00:21:35 +0100

    feat: remove unused import of plotly.express in Global Inflation page

--- src/dashboard/pages/🌍_Global_Inflation.py
+++ src/dashboard/pages/🌍_Global_Inflation.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.express as px
 
 st.set_page_config(layout="wide", page_title="Global Inflation", page_icon="🌍")
 
